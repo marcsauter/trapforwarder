c3c82052888b5e2e164281f5d0c0c747e02c77bf
Marc Sauter <marc.sauter@postfinance.ch>
2016-02-26 11:07:13 +0000

    status mapping

--- sensu/sensu.go
+++ sensu/sensu.go
@@ -23,9 +23,34 @@ func NewSensu(t *trap.Trap) *Sensu {
 	s.Name = t.Vars["SNMPv2-SMI::enterprises.5471.2.20.15"]
 	switch t.Vars["SNMPv2-SMI::enterprises.5471.2.20.10"] {
 	case "0":
-		s.Status = 0
-		if strings.ToUpper(t.Vars["SNMPv2-SMI::enterprises.5471.2.20.20"]) == "NORMAL" {
+		switch strings.ToUpper(t.Vars["SNMPv2-SMI::enterprises.5471.2.20.20"]) {
+		case "NORMAL":
 			s.Status = 0
+		case "WARNING":
+			s.Status = 1
+		case "CRITICAL":
+			s.Status = 2
+		default:
+			s.Status = 3
+		}
+		msg = fmt.Sprintf("node=%s", t.Vars["SNMPv2-SMI::enterprises.5471.2.20.15"])
+		msg = fmt.Sprintf("%s;severity=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.20"])
+		msg = fmt.Sprintf("%s;application=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.25"])
+		msg = fmt.Sprintf("%s;object=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.30"])
+		msg = fmt.Sprintf("%s;msg_grp=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.35"])
+		msg = fmt.Sprintf("%s;msg_text=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.40"])
+		msg = fmt.Sprintf("%s;service_id=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.45"])
+		s.Output = msg
+	case "1":
+		switch strings.ToUpper(t.Vars["SNMPv2-SMI::enterprises.5471.2.20.20"]) {
+		case "OK":
+			s.Status = 0
+		case "WARNING":
+			s.Status = 1
+		case "CRITICAL":
+			s.Status = 2
+		default:
+			s.Status = 3
 		}
 		msg = fmt.Sprintf("host=%s", t.Vars["SNMPv2-SMI::enterprises.5471.2.20.15"])
 		msg = fmt.Sprintf("%s;severity=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.20"])
@@ -40,21 +65,6 @@ func NewSensu(t *trap.Trap) *Sensu {
 		msg = fmt.Sprintf("%s;alarm_id=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.65"])
 		msg = fmt.Sprintf("%s;dedup_key=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.70"])
 		msg = fmt.Sprintf("%s;security_event=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.75"])
-		s.Output = msg
-	case "1":
-		s.Status = 0
-		if strings.ToUpper(t.Vars["SNMPv2-SMI::enterprises.5471.2.20.20"]) == "OK" {
-			s.Status = 0
-		}
-		msg = fmt.Sprintf("host=%s", t.Vars["SNMPv2-SMI::enterprises.5471.2.20.15"])
-		msg = fmt.Sprintf("%s;severity=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.20"])
-		msg = fmt.Sprintf("%s;object_class=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.25"])
-		msg = fmt.Sprintf("%s;object=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.30"])
-		msg = fmt.Sprintf("%s;parameter=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.35"])
-		msg = fmt.Sprintf("%s;parameter_value=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.40"])
-		msg = fmt.Sprintf("%s;message=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.45"])
-		msg = fmt.Sprintf("%s;tc=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.50"])
-		msg = fmt.Sprintf("%s;ac=%s", msg, t.Vars["SNMPv2-SMI::enterprises.5471.2.20.55"])
 		s.Output = msg
 	default:
 		s.Output = fmt.Sprintf("UNKNOWN VERSION: %s", t.Vars["SNMPv2-SMI::enterprises.5471.2.20.10"])
